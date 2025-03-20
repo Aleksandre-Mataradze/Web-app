9b5b73579612f4da67ba5a9b0dae1ec5e7920993
TURA999 <mataradze.alex@gmail.com>
2025-03-20 16:25:32 +0400

    Added random products for main page from JSON

--- main.js
+++ main.js
@@ -1,26 +1,52 @@
+function convertToBase64(file) {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      console.log(reader.result); // This is the Base64 string
+    };
+  }
+  
+
 fetch('https://api.everrest.educata.dev/shop/products/all?page_index=1&page_size=38')
 .then((response) => response.json())
 .then((data) => {
     console.log("data", data)
     let productIndexArr = []; // Array for the Card list.
-    for(let i=1; i<=4; i++){ // gets up to 4 random index from product array.
-        let randomProductIndex = Math.floor(Math.random() * (data.products.length - 0) + 0);
+    while (productIndexArr.length != 5){
+        let randomProductIndex = Math.floor(Math.random() * (data.products.length - 0) + 0); // Change both 0 to change min value
+        if(productIndexArr.includes(randomProductIndex)){
+            randomProductIndex = Math.floor(Math.random() * (data.products.length - 0) + 0); // Change both 0 to change min value
+        }
         productIndexArr.push(randomProductIndex);
-    } // Can return same two or more same index
+    }
 
     for(let i=0; i<data.products.length; i++){
         if(productIndexArr.includes(i)){
-            // productIndexArr.forEach(element => {
             let card = document.createElement('div');
             card.classList.add("card");
             let laptopCardList = document.querySelector(".laptop-card-list");
             laptopCardList.appendChild(card);
-            console.log(card)
+            console.log(card);
+
             let cardImg = document.createElement('img');
-            cardImg.setAttribute('src', data.products[i].thumbnail)
-            card.appendChild(cardImg)
-            // console.log(data)
-        // })
+            cardImg.setAttribute('src', data.products[i].thumbnail);
+            card.appendChild(cardImg);
+
+            let cardTitle = data.products[i].title;
+            let h5 = document.createElement('h5');
+            h5.textContent = cardTitle;
+            console.log(h5);
+            card.appendChild(h5);
+
+            let currentPrice = data.products[i].price.current;
+            if(data.products[i].price.currency == 'USD'){ // converts USD to GEL
+                currentPrice *= 2.77; 
+            }
+            const pricePara = document.createElement('p');
+            pricePara.classList.add('price')
+            pricePara.textContent = `${Math.round(currentPrice)}ლ`;
+            card.appendChild(pricePara)
         }
     }
 })
+
